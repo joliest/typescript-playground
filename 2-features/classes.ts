184dbf7f9f789f0ac8d57f8dee62a0359c28fe97
Joliver Estampador <joliverestampador@gmail.com>
2022-07-12 07:35:55 +0800

    modifiers

--- 2-features/classes.ts
+++ 2-features/classes.ts
@@ -1,19 +1,22 @@
+// you wont be able to change the modifier of the subclass with the same method name
 class Vehicle {
-  drive(): void {
-    console.log('chuga chuga');
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  // restrict different methods that other developer can call that could break something
+  private drive(): void {
     console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
-car.drive();
-car.honk();
+car.startDrivingProcess();
+// car.honk();
