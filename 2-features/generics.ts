2eda263dd5209548f9e44e73627cdcc6507e1676
Joliver Estampador <joliverestampador@gmail.com>
2022-08-12 08:00:49 +0800

    Generics with functions
    Always add the type annotation of generics in functions and class (even if the type inference able to detect them)
    This will always help us to catch errors

--- 2-features/generics.ts
+++ 2-features/generics.ts
@@ -26,3 +26,24 @@ class ArrayOfAnything<T> {
 }
 
 const arr = new ArrayOfAnything(['a', 'b', 'c']);
+
+// example of generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c']);
