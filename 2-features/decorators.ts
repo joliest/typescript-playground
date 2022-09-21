58cc08f5294b44a178a87597c0188e2add95c42a
Joliver Estampador <joliverestampador@gmail.com>
2022-09-21 08:39:56 +0800

    Other uses of decorators

--- 2-features/decorators.ts
+++ 2-features/decorators.ts
@@ -1,14 +1,36 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
   @logError('Oops the boat was sunk!')
-  pilot(): void {
-    throw new Error();
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log('parameterDecorator :::', key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
 }
 
 function logError(errorMessage: string) {
@@ -25,5 +47,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
