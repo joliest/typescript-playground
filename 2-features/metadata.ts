0113f31ae5a5f628f5c67121e5381f0ba2ff9e3c
Joliver Estampador <joliverestampador@gmail.com>
2022-09-27 07:49:49 +0800

    associating metadata to a property

--- 2-features/metadata.ts
+++ 2-features/metadata.ts
@@ -4,6 +4,11 @@ const plane = {
   color: 'red',
 };
 
-Reflect.defineMetadata('note', 'hi there', plane);
-const note = Reflect.getMetadata('note', plane);
+// Reflect.defineMetadata('note', 'hi there', plane);
+// const note = Reflect.getMetadata('note', plane);
+// console.log(note);
+
+// associating metadata to the color property
+Reflect.defineMetadata('note', 'hi there', plane, 'color');
+const note = Reflect.getMetadata('note', plane, 'color');
 console.log(note);
