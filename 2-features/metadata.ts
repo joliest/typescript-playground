5dc9fca16a923775d40074a9edefb0157fbe0d51
Joliver Estampador <joliverestampador@gmail.com>
2022-09-27 08:05:31 +0800

    applying metadata to class
    Start here https://docs.google.com/document/d/1Wj4SYC_Anp0kn8_x8C00zxUndSaePL0SuJV5LQ4d_k4/edit#heading=h.8enlcdtvg6a2

--- 2-features/metadata.ts
+++ 2-features/metadata.ts
@@ -1,14 +1,39 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('hi there')
+  fly(): void {
+    console.log('vrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+// typeof Plane is a reference of the constructor() of the class
+function printMetadata(target: typeof Plane) {
+  // key will be fly
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
+
+// const plane = {
+//   color: 'red',
+// };
 
 // Reflect.defineMetadata('note', 'hi there', plane);
 // const note = Reflect.getMetadata('note', plane);
 // console.log(note);
 
 // associating metadata to the color property
-Reflect.defineMetadata('note', 'hi there', plane, 'color');
-const note = Reflect.getMetadata('note', plane, 'color');
-console.log(note);
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+// const note = Reflect.getMetadata('note', plane, 'color');
+// console.log(note);
